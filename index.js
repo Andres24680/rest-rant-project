da85819561b82fdd8df098d0d20416c213b8adf6
Andres24680 <tasboyandresrestrepo@aol.com>
2022-12-14 09:38:45 -0500

    adding gitignore files

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const express = require('express')
 const app = express()
+
+app.get('/', function (req, res){
+    res.send('Hello world')      // this is what is seen at browser     
+})
+
+app.listen(3000)
