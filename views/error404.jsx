2026cf81c6ae7cbe43c636a0ca909177396300f0
Andres24680 <tasboyandresrestrepo@aol.com>
2022-12-18 12:02:39 -0500

    adding 404 image to path

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,9 @@ function error404 () {
     <Def>
         <main>
             <h1>404: PAGE NOT FOUND</h1>
+            <div>
+                <img src="/images/404 project page img.jpeg" alt="404-image" />
+            </div>
             <p>Oops, sorry, we can't find this page!</p>
         </main>
     </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -7,8 +7,11 @@ function home() {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="/images/404 project page img.jpeg" alt="404 imag" id="404 img"/>
+            </div>
             <a href="/places">
-  <button className="btn-primary">Places Page</button>
+  <button className="btn btn-primary">Places Page</button>
 </a>
 
         </main>
