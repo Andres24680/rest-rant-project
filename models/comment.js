dbd46de0c2ec978bdc83e2152a60d36abb49f44e
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-08 13:44:40 -0500

    added comment section to show page

---
+++ models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+//comment schema 
+let commentSchema = new mongoose.Schema({
+    author: {type: String, default: 'Anonymous'}, 
+    rant: { type: Boolean, default: false}, 
+    stars: { type: Number, required: true}, 
+    content: {type: String, default:''}
+})
+
+
+//export
+module.export = mongoose.model('Comment', commentSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,28 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data){
+    let comments = (
+        <h3 className="inactive">
+        No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length){
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border col-sm-4" key={c._id}>
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                    <form action={`/places/${data.place._id}/comment/${c._id}?_method=DELETE`} method="POST">
+                        <button type="submit" className="btn btn-danger">Delete Comment</button>
+                    </form>
+                </div>
+            )
+        })
+    }
   return (
     <Def>
          <main>
@@ -27,6 +49,7 @@ function show(data){
                      </form> 
                  </div>
                  <h2>Comments</h2>
+                 {comments}
                  <div className="row">
 
 
