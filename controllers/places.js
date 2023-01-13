c76ee005d98ca3fc64e7d8fa496c90cce9694bd0
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-12 20:14:38 -0500

    comment section working

--- controllers/places.js
+++ controllers/places.js
@@ -88,7 +88,7 @@ router.post('/:id/comment', (req, res) => {
   db.Place.findById(req.params.id)
   .then(place => {
 
-      db.Comment.save(req.body)
+      db.Comment.create(req.body)
       .then(comment => {
           place.comments.push(comment.id)
           place.save()
